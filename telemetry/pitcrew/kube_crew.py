b1308bef8b6a0f5f99de0790c3e87beafdc5d4f8
Marcel Hild <hild@b4mad.net>
2024-05-13 19:17:23 +0200

    proper driver names

--- telemetry/pitcrew/kube_crew.py
+++ telemetry/pitcrew/kube_crew.py
@@ -1,8 +1,11 @@
 import logging
 import os
 import re
-from kubernetes import client, config as kubeconfig
+
 import yaml
+from kubernetes import client
+from kubernetes import config as kubeconfig
+
 
 class KubeCrew:
     def __init__(self):
@@ -13,7 +16,7 @@ class KubeCrew:
             kubeconfig.load_incluster_config()
 
         # get the namespace from the environment
-        self.namespace = os.environ.get('POD_NAMESPACE', 'b4mad-racing')
+        self.namespace = os.environ.get("POD_NAMESPACE", "b4mad-racing")
         # Read the YAML file
         path = os.path.dirname(os.path.abspath(__file__))
         with open(f"{path}/deployment_pitcrew.yaml", "r") as file:
@@ -29,7 +32,7 @@ class KubeCrew:
         # and must start and end with an alphanumeric character
         # regexp: '[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*'
 
-        return re.sub(r'[^a-zA-Z0-9]+', '-', name).lower()
+        return re.sub(r"[^a-zA-Z0-9]+", "-", name).lower()
 
     def stop_coach(self, driver_name):
         namespace = self.namespace
@@ -40,7 +43,7 @@ class KubeCrew:
         v1 = client.AppsV1Api()
         try:
             api_response = v1.delete_namespaced_deployment(namespace=namespace, name=name)
-            logging.info(f"Deployment deleted.")
+            logging.info("Deployment deleted.")
             logging.debug(f"Deployment deleted. status={api_response}")
             return True
         except client.rest.ApiException as e:
@@ -54,15 +57,15 @@ class KubeCrew:
         v1 = client.AppsV1Api()
 
         deployment = self.deployment_obj
-        deployment.metadata['name'] = name
-        deployment.metadata['labels']['app.kubernetes.io/component'] = name
-        deployment.metadata['labels']['b4mad.racing/driver'] = name
-        deployment.metadata['labels']['b4mad.racing/component'] = 'pitcrew'
-        deployment.spec['selector']['matchLabels']['app.kubernetes.io/component'] = name
-        deployment.spec['template']['metadata']['labels']['app.kubernetes.io/component'] = name
+        deployment.metadata["name"] = name
+        deployment.metadata["labels"]["app.kubernetes.io/component"] = name
+        deployment.metadata["labels"]["b4mad.racing/driver"] = name
+        deployment.metadata["labels"]["b4mad.racing/component"] = "pitcrew"
+        deployment.spec["selector"]["matchLabels"]["app.kubernetes.io/component"] = name
+        deployment.spec["template"]["metadata"]["labels"]["app.kubernetes.io/component"] = name
 
         # set spec.template.spec.containers[0].env[-1].value to the driver name
-        deployment.spec['template']['spec']['containers'][0]['env'][-1]['value'] = driver_name
+        deployment.spec["template"]["spec"]["containers"][0]["env"][-1]["value"] = driver_name
 
         logging.info(f"Creating deployment {name} in namespace {namespace}")
         try:
@@ -111,8 +114,6 @@ class KubeCrew:
             driver_name = deployment.replace("pitcrew-", "")
             if driver_name not in sanitized_drivers:
                 self.stop_coach(driver_name)
-            else:
-                sanitized_drivers.remove(driver_name)
         # start deployments that are in self.drivers
-        for driver_name in sanitized_drivers:
+        for driver_name in self.drivers:
             self.start_coach(driver_name)
