fe4bc06d84987d1a1fdd5f9be814333b5cb70466
Marcel Hild <hild@b4mad.net>
2024-06-01 10:45:20 +0200

    adjust sentry sample rate

--- paddock/settings.py
+++ paddock/settings.py
@@ -339,9 +339,9 @@ if sentry_dsn and sentry_dsn.startswith("http"):
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production.
-        traces_sample_rate=1.0,
+        traces_sample_rate=0.1,
         # Set profiles_sample_rate to 1.0 to profile 100%
         # of sampled transactions.
         # We recommend adjusting this value in production.
-        profiles_sample_rate=1.0,
+        profiles_sample_rate=0.1,
     )
